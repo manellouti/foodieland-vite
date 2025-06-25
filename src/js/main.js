f448313d5ce367fba9fee1061d58d413d2304c86
Manel <manellouti@live.fr>
2025-06-25 13:52:12 +0200

    Add tasty recipes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 // src/main.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // ← indispensable
 import '../css/style.css';
